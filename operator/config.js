1c9d34280353636d0a2693f563f9398da3c84f54
market-predictions <darko.desktop@gmail.com>
2026-08-29 14:42:55 +0200

    feat: make internal and public prospect origins configurable

--- operator/config.js
+++ operator/config.js
@@ -1,6 +1,9 @@
 window.SOLIDDESIGN_OPERATOR_CONFIG = Object.freeze({
   supabaseUrl: "https://grderdhnjkeucaaehgqy.supabase.co",
-  supabasePublishableKey: "sb_publishable_fRXRtDIHJ98LIN3cfQHtpA_WJ0yPPRh"
+  supabasePublishableKey: "sb_publishable_fRXRtDIHJ98LIN3cfQHtpA_WJ0yPPRh",
+  internalOrigin: "https://soliddesign-cms.pages.dev",
+  publicProspectOrigin: "https://soliddesign-cms.pages.dev",
+  publicProspectPathPrefix: "/prospect"
 });
 
 import('./prospect-link.js').catch((error) => console.error('Prospectlink module kon niet laden.', error));
