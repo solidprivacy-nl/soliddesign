a40b669eb922e7b7b1f2e2ad8be71eee77dd3e95
market-predictions <darko.desktop@gmail.com>
2026-08-29 15:40:17 +0200

    feat: load external preview publication policy

--- operator/config.js
+++ operator/config.js
@@ -10,5 +10,6 @@ import('./invite-setup.js').catch((error) => console.error('Uitnodigingsflow kon
 import('./prospect-link.js').catch((error) => console.error('Prospectlink module kon niet laden.', error));
 import('./team-work.js').catch((error) => console.error('Teamwerk module kon niet laden.', error));
 import('./prospect-work-filter.js').catch((error) => console.error('Werkverdelingsfilter kon niet laden.', error));
+import('./mockup-policy.js').catch((error) => console.error('Mock-up publicatiebeleid kon niet laden.', error));
 import('./dossier-tabs.js').catch((error) => console.error('Dossierweergave kon niet laden.', error));
 import('./engagement-ui.js').catch((error) => console.error('Digitale respons kon niet laden.', error));
